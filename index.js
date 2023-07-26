51c31897f8650ee0f0bdbd48f6a085a002a6b24d
KampfZomby <kampfzomby@gmail.com>
2023-07-27 00:20:01 +0200

    modified:   Edited Meta Data

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use('',express.static(path.join(__dirname, 'public')));
+app.use('', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (request, response) => {
-	return response.sendFile('index.html', { root: '.' });
+    return response.sendFile('index.html', { root: '.' });
 });
 
 app.get('/datenschutz', (request, response) => {
-	return response.sendFile('/public/datenschutz.html', { root: '.' });
+    return response.sendFile('/public/datenschutz.html', { root: '.' });
 });
 
 app.get('/impressum', (request, response) => {
-	return response.sendFile('/public/impressum.html', { root: '.' });
+    return response.sendFile('/public/impressum.html', { root: '.' });
 });
 
 app.get('/play', (request, response) => {
-	return response.sendFile('/sudoku/sudoku.html', { root: '.' });
+    return response.sendFile('/sudoku/sudoku.html', { root: '.' });
 });
 
 app.get('/rooms', (request, response) => {
-	return response.sendFile('/multiplayer/sudoku-versus.html', { root: '.' });
+    return response.sendFile('/multiplayer/sudoku-versus.html', { root: '.' });
 });
 
 app.get('/game', (request, response) => {
-	return response.sendFile('/multiplayer/sudoku.html', { root: '.' });
+    return response.sendFile('/multiplayer/sudoku.html', { root: '.' });
 });
 
 app.get('/print', (request, response) => {
-	return response.sendFile('/print-sudoku/sudoku.html', { root: '.' });
+    return response.sendFile('/print-sudoku/sudoku.html', { root: '.' });
 });
 
 app.listen(6969);
